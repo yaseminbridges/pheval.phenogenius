ddbd69704641f335b417ad66254a3a5b98704557
Yasemin Bridges <y.bridges@qmul.ac.uk>
2024-11-26 12:59:18 +0000

    implement runner

--- src/pheval_phenogenius/runner.py
+++ src/pheval_phenogenius/runner.py
@@ -5,6 +5,9 @@ from pathlib import Path
 
 from pheval.runners.runner import PhEvalRunner
 
+from pheval_phenogenius.post_process.post_process import post_process_results_format
+from pheval_phenogenius.run.run import run
+
 
 @dataclass
 class PhEvalPhenoGeniusRunner(PhEvalRunner):
@@ -24,7 +27,17 @@ class PhEvalPhenoGeniusRunner(PhEvalRunner):
     def run(self):
         """Run."""
         print("running")
+        run(
+            testdata_dir=self.testdata_dir,
+            input_dir=self.input_dir,
+            raw_results_dir=self.raw_results_dir,
+            tool_input_commands_dir=self.tool_input_commands_dir,
+        )
 
     def post_process(self):
         """Post Process."""
         print("post processing")
+        post_process_results_format(
+            raw_results_dir=self.raw_results_dir,
+            output_dir=self.output_dir,
+        )
